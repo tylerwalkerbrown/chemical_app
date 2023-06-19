28b13e9a31a2dd2a0be04f0102d679790ceec157
Tyler Brown <94020684+tylerwalkerbrown@users.noreply.github.com>
2023-06-19 19:34:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import pandas as pd
 import streamlit as st
 from geopy.geocoders import Nominatim
 
-# Function to process the uploaded CSV file
 def process_csv(file):
     df = pd.read_csv(file)
     df['State'] = df['siteaddress'].str.split(",", expand=True)[1].str.split(" ", expand=True)[1]
@@ -27,19 +26,6 @@ def process_csv(file):
     df['week'] = df['date'].dt.isocalendar().week
     return df
 
-def pivot_summary_nh(data):
-    data['date'] = pd.to_datetime(data['completeddate'])
-    data['week'] = data['date'].dt.isocalendar().week
-
-    pivot_table = data.pivot_table(values='materialquantity', index='week', columns='itemnum', aggfunc='sum')
-
-    # Add the date next to the week number in the pivot table
-    week_dates = pd.Series(pd.to_datetime(data.groupby('week')['date'].min()))
-    week_dates = week_dates.dt.strftime('%Y-%m-%d')
-    pivot_table.insert(0, 'Date', week_dates)
-
-    return pivot_table
-
 def pivot_summary_vt(data):
     data['date'] = pd.to_datetime(data['completeddate'])
     data['week'] = data['date'].dt.isocalendar().week
@@ -51,14 +37,52 @@ def pivot_summary_vt(data):
     week_dates = week_dates.dt.strftime('%Y-%m-%d')
     pivot_table.insert(0, 'Date', week_dates)
 
+    if not pivot_table.empty:
+        # Calculate Square Footage by multiplying the sum of material quantity by 5000
+        pivot_table['Square Footage'] = data.groupby('week')['materialquantity'].sum() * 5000
+
     return pivot_table
 
-# Streamlit app
+
+def pivot_summary_nh(data):
+    data['date'] = pd.to_datetime(data['completeddate'])
+    data['week'] = data['date'].dt.isocalendar().week
+
+    pivot_table = data.pivot_table(values='materialquantity', index='week', columns='itemnum', aggfunc='sum')
+
+    # Apply multipliers to specific columns
+    multipliers = {
+        'Essentria IC3': 2,
+        'Cyzmic': 0.8,
+        'Pivot10': 4,
+        'Stryker': 10,
+        'Surf-AC': 4,
+        'Tekko Pro': 1,
+        'DuraFlex ZC': 1.5,
+        'STB - 6 oz': 4,
+        'STB - 4 oz': 4
+    }
+
+    for column, multiplier in multipliers.items():
+        if column in pivot_table.columns:
+            pivot_table[column] *= multiplier
+
+    # Add the date next to the week number in the pivot table
+    week_dates = pd.Series(pd.to_datetime(data.groupby('week')['date'].min()))
+    week_dates = week_dates.dt.strftime('%Y-%m-%d')
+    pivot_table.insert(0, 'Date', week_dates)
+
+    if not pivot_table.empty:
+        # Calculate Square Footage by multiplying the sum of material quantity by 5000
+        pivot_table['Square Footage'] = data.groupby('week')['materialquantity'].sum() * 5000
+
+    return pivot_table
+
+
 def main():
     st.title("CSV Analysis")
     st.sidebar.title("Options")
 
-    # Display requirements page
     if st.sidebar.checkbox("View Requirements"):
         st.subheader("Requirements")
         st.markdown("""
@@ -69,43 +93,25 @@ def main():
         - The `itemnum` column should contain the item number.
         """)
 
-    # Allow file upload
     file = st.file_uploader("Upload CSV", type=["csv"])
 
     if file is not None:
-        # Process the uploaded CSV file
         df = process_csv(file)
-
-        # Display pivot table for all states
-        st.subheader("Pivot Table for All States")
-        pivot_table = df.pivot_table(values='materialquantity', index='week', columns='itemnum', aggfunc='sum')
-        st.write(pivot_table)
-
-        # Download CSV for pivot table of all states
-        csv_all_states = pivot_table.to_csv(index=True)
-        st.download_button("Download CSV (All States)", data=csv_all_states, file_name="pivot_table_all_states.csv")
-
-        # Filter data for NH
         data_nh = df[df['State'] == 'NH']
 
-        # Display pivot table for NH
         st.subheader("Pivot Table for NH")
         pivot_table_nh = pivot_summary_nh(data_nh)
         st.write(pivot_table_nh)
 
-        # Download CSV for pivot table of NH
         csv_nh = pivot_table_nh.to_csv(index=True)
         st.download_button("Download CSV (NH)", data=csv_nh, file_name="pivot_table_nh.csv")
 
-        # Filter data for VT
         data_vt = df[df['State'] == 'VT']
 
-        # Display pivot table for VT
         st.subheader("Pivot Table for VT")
         pivot_table_vt = pivot_summary_vt(data_vt)
         st.write(pivot_table_vt)
 
-        # Download CSV for pivot table of VT
         csv_vt = pivot_table_vt.to_csv(index=True)
         st.download_button("Download CSV (VT)", data=csv_vt, file_name="pivot_table_vt.csv")
 
